0b9e2705e80f04ed42fcc2169dc3be754cf21168
Purity <66000596+graalpurity@users.noreply.github.com>
2021-02-06 23:26:04 -0500

    Add files via upload

---
+++ Forms/RPC.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace Resident_Evil_2_RPC
+{
+    partial class RPC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.exitBtn = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.miniBtn = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.leonBtn = new Guna.UI2.WinForms.Guna2Button();
+            this.claireBtn = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.characterLabel = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.dragControl = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.SuspendLayout();
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.exitBtn.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.exitBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.exitBtn.HoverState.Parent = this.exitBtn;
+            this.exitBtn.IconColor = System.Drawing.Color.White;
+            this.exitBtn.Location = new System.Drawing.Point(767, 2);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.exitBtn.ShadowDecoration.Parent = this.exitBtn;
+            this.exitBtn.Size = new System.Drawing.Size(45, 29);
+            this.exitBtn.TabIndex = 0;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // miniBtn
+            // 
+            this.miniBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.miniBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.miniBtn.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.miniBtn.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.miniBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.miniBtn.HoverState.Parent = this.miniBtn;
+            this.miniBtn.IconColor = System.Drawing.Color.White;
+            this.miniBtn.Location = new System.Drawing.Point(716, 2);
+            this.miniBtn.Name = "miniBtn";
+            this.miniBtn.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.miniBtn.ShadowDecoration.Parent = this.miniBtn;
+            this.miniBtn.Size = new System.Drawing.Size(45, 29);
+            this.miniBtn.TabIndex = 1;
+            // 
+            // leonBtn
+            // 
+            this.leonBtn.CheckedState.Parent = this.leonBtn;
+            this.leonBtn.CustomImages.Parent = this.leonBtn;
+            this.leonBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.leonBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.leonBtn.ForeColor = System.Drawing.Color.White;
+            this.leonBtn.HoverState.Parent = this.leonBtn;
+            this.leonBtn.Location = new System.Drawing.Point(-9, 66);
+            this.leonBtn.Name = "leonBtn";
+            this.leonBtn.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.leonBtn.ShadowDecoration.Parent = this.leonBtn;
+            this.leonBtn.Size = new System.Drawing.Size(130, 45);
+            this.leonBtn.TabIndex = 2;
+            this.leonBtn.Text = "Leon";
+            this.leonBtn.Click += new System.EventHandler(this.leonBtn_Click);
+            // 
+            // claireBtn
+            // 
+            this.claireBtn.CheckedState.Parent = this.claireBtn;
+            this.claireBtn.CustomImages.Parent = this.claireBtn;
+            this.claireBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.claireBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.claireBtn.ForeColor = System.Drawing.Color.White;
+            this.claireBtn.HoverState.Parent = this.claireBtn;
+            this.claireBtn.Location = new System.Drawing.Point(127, 66);
+            this.claireBtn.Name = "claireBtn";
+            this.claireBtn.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.claireBtn.ShadowDecoration.Parent = this.claireBtn;
+            this.claireBtn.Size = new System.Drawing.Size(130, 45);
+            this.claireBtn.TabIndex = 3;
+            this.claireBtn.Text = "Claire";
+            this.claireBtn.Click += new System.EventHandler(this.claireBtn_Click);
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.White;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(0, 38);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(77, 22);
+            this.guna2HtmlLabel1.TabIndex = 4;
+            this.guna2HtmlLabel1.Text = "Character:";
+            // 
+            // characterLabel
+            // 
+            this.characterLabel.BackColor = System.Drawing.Color.Transparent;
+            this.characterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.characterLabel.ForeColor = System.Drawing.Color.White;
+            this.characterLabel.Location = new System.Drawing.Point(83, 38);
+            this.characterLabel.Name = "characterLabel";
+            this.characterLabel.Size = new System.Drawing.Size(217, 22);
+            this.characterLabel.TabIndex = 5;
+            this.characterLabel.Text = "Waiting for character selection";
+            // 
+            // dragControl
+            // 
+            this.dragControl.ContainerControl = this;
+            this.dragControl.TargetControl = this;
+            // 
+            // RPC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ClientSize = new System.Drawing.Size(814, 470);
+            this.Controls.Add(this.characterLabel);
+            this.Controls.Add(this.guna2HtmlLabel1);
+            this.Controls.Add(this.claireBtn);
+            this.Controls.Add(this.leonBtn);
+            this.Controls.Add(this.miniBtn);
+            this.Controls.Add(this.exitBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "RPC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2ControlBox exitBtn;
+        private Guna.UI2.WinForms.Guna2ControlBox miniBtn;
+        public Guna.UI2.WinForms.Guna2Button leonBtn;
+        public Guna.UI2.WinForms.Guna2Button claireBtn;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        public Guna.UI2.WinForms.Guna2HtmlLabel characterLabel;
+        public Guna.UI2.WinForms.Guna2DragControl dragControl;
+    }
+}
+
---
+++ Forms/RPC.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Resident_Evil_2_RPC.Handler;
+using static Resident_Evil_2_RPC.Handler.Discord;
+
+namespace Resident_Evil_2_RPC
+{
+    public partial class RPC : Form
+    {
+        public static bool leon = false;
+        public static bool claire = false;
+        public static bool discordstarted = false;
+
+        public RPC()
+        {
+            InitializeComponent();
+            Discord.ID();
+            Discord.Start();
+        }
+
+        private void leonBtn_Click(object sender, EventArgs e)
+        {
+            if (discordstarted == false)
+            {
+                claire = false;
+                leon = true;
+                Discord.LeonStart();
+                discordstarted = true;
+            }
+            else
+            {
+                claire = false;
+                leon = true;
+                Discord.Leon();
+                discordstarted = true;
+            }
+        }
+
+        private void claireBtn_Click(object sender, EventArgs e)
+        {
+            if (discordstarted == false)
+            {
+                claire = true;
+                leon = false;
+                Discord.ClaireStart();
+                discordstarted = true;
+            }
+            else
+            {
+                claire = true;
+                leon = false;
+                Discord.Claire();
+            }
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Discord.Dispose();
+        }
+    }
+}
